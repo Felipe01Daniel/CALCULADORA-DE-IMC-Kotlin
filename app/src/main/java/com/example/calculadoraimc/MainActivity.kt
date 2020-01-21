6b0cc8c309fbe0671bf5b6735070febdc1e670f7
FDanielsNX1 <fdaniels.it@gmail.com>
2020-01-21 08:24:47 -0300

    Criando Spinner Lista de Sexo, e layout para o spinner

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.calculadoraimc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //Spinner Lista de Sexo
+        val listaSexo = arrayListOf("     Selecione o Sexo", "Feminino", "Masculino")
+        val adapterSexo = ArrayAdapter(this@MainActivity, R.layout.spinner_item,listaSexo)
+        adapterSexo.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spnSexo.adapter = adapterSexo
+
+
+        //Button Calcular
         btnCalcular.setOnClickListener {
             startActivity(Intent(this@MainActivity, ResultadoActivity::class.java))
         }
 
+        //Button Tabela
         btnTabela.setOnClickListener {
             startActivity(Intent(this@MainActivity, TabelaActivity::class.java))
         }

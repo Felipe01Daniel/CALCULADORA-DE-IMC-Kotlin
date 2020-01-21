3a6d7490ee3f1d98ffc8ecb9d1b521e1a7cfc4ef
FDanielsNX1 <fdaniels.it@gmail.com>
2020-01-21 05:49:39 -0300

    Criando Telas do app

---
+++ app/src/main/java/com/example/calculadoraimc/TabelaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TabelaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabela)
+    }
+}
